41092f2d6a37dfcf8bf516871cbcfe108dd01608
Teodor Zafirov <36457245+t3dkich@users.noreply.github.com>
2024-02-23 17:58:19 +0200

    signature verification successful

--- programs/solana-treasury/src/lib.rs
+++ programs/solana-treasury/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
-use solana_program::secp256k1_recover::{secp256k1_recover};
-use k256;
-use k256::ecdsa::signature::Verifier;
+use anchor_lang::system_program::{transfer, Transfer};
+use solana_program::secp256k1_recover::secp256k1_recover;
+use solana_program::hash::hash;
 
 declare_id!("3PxEqHbGAWZthkyuJvtTjjwYSQ3HRGJVyk8MbjnZP2mt");
 
@@ -9,69 +9,154 @@ declare_id!("3PxEqHbGAWZthkyuJvtTjjwYSQ3HRGJVyk8MbjnZP2mt");
 pub mod solana_treasury {
     use super::*;
 
-    pub fn verify(
-        ctx: Context<New>,
-        // signature_hex: String,
-        // message: String,
-        // public_key_hex: String,
-    ) -> Result<()> {
-        let public_key_hex = "026f0a0eac8db33cebd88516ccbfafc148cd97abfe217e456c4f47f0478b1b443d";
+    pub fn deposit(ctx: Context<DepositCtx>, amount: u64) -> Result<()> {
+        transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.payer.to_account_info(),
+                    to: ctx.accounts.treasury.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
 
-        let message = "69ee7a3423fa9be01fa80a47dffc9c3135fdccaab0ca5d5c4fbc3459b66c271e";
-
-        let signature_hex = "5bf05d6ede6316f675a3d0ad0896db0473aaa6084a1c373046fc5ea2a285edd81b0dfc8c5bb6bb0affff58fbc4c16f73b757414b0af05ab4bc139864903bb81b";
-        
-        let signature_bytes = hex::decode(&signature_hex).expect("failed to hex-decode signature");
-        let pubkey_bytes = hex::decode(&public_key_hex).expect("failed to hex-decode public key");
-        let message_bytes = message.as_bytes();
-
-        let signature = k256::ecdsa::Signature::try_from(signature_bytes.as_slice())
-            .expect("failed to deserialize signature");
-        let is_signature_valid = k256::ecdsa::VerifyingKey::from_sec1_bytes(&pubkey_bytes)
-            .expect("failed to deserialize sec1 encoding into public key")
-            .verify(message_bytes, &signature)
-            .is_ok();
+        emit!(DepositEvent {
+            sender: *ctx.accounts.payer.to_account_info().key,
+            amount: amount,
+        });
 
         Ok(())
     }
 
-    pub fn recover(ctx: Context<New>) -> Result<()> {
-        let eth_pubkey = "026f0a0eac8db33cebd88516ccbfafc148cd97abfe217e456c4f47f0478b1b443d";
+    pub fn withdraw(ctx: Context<WithdrawCtx>, amount: u64) -> Result<()> {
+        // PDA signer seeds
+        let signer_seeds: &[&[&[u8]]] = &[&[b"treasury", &[ctx.bumps.treasury]]];
+    
+        // Create the new account, transferring lamports from the rent vault to the new account
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.treasury.to_account_info(),
+                    to: ctx.accounts.receiver.to_account_info(),
+                },
+                signer_seeds
+            ),
+            amount,
+        )?;
 
-        // ethers.solidityPackedKeccak256(['string'],["some_message"])
-        let message = "69ee7a3423fa9be01fa80a47dffc9c3135fdccaab0ca5d5c4fbc3459b66c271e";
+        Ok(())
+    }
 
-        // Buffer.concat([sig.r, sig.s]).toString('hex'))
-        let signature = "5bf05d6ede6316f675a3d0ad0896db0473aaa6084a1c373046fc5ea2a285edd81b0dfc8c5bb6bb0affff58fbc4c16f73b757414b0af05ab4bc139864903bb81b";
-        
-        // sig.v - 27
-        let recovery_id = 1;
+    const ETH_PUBKEY: [u8; 64] = [
+        103, 195,  88, 222,  59, 129,  88,  22, 117, 109, 111,
+         44, 176,  82,  12, 162,  43, 102,  11,  40,  45, 232,
+        229, 153, 222, 131, 162, 180, 187,  47,  96, 171, 145,
+        123, 189, 254,  81,  78, 224, 252,  29, 180, 157,  53,
+        206, 225,  54, 199,   6, 242, 238, 217,  95, 117, 170,
+         68,  71, 185, 108, 206, 181, 188, 172,  87
+    ];
+    
+    pub fn verify(_ctx: Context<New>, data: VerificationData) -> Result<()> {
+        let message = format!(
+            r#"{{"address":"{}","amount":{}}}"#,
+            data.verify_data.address.clone(),
+            data.verify_data.amount
+        );
 
-        let recovered_pubkey = secp256k1_recover(
-            &message.as_bytes(),
-            recovery_id,
-            &signature.as_bytes(),
-        )
-        .map_err(|_| ProgramError::InvalidArgument)?;
+        let msg_data_hashed = hash(&message.as_bytes());
 
-        // 58a8a3bd63db626497e1b4c09fb3119257c2bd0c651084eac0c038ad49bec874e380efbb5f8232aceae2383b9a1bbc22e936121ebaf83ff880801232327380e3
-        let recovered_pubkey_hex = hex::encode(recovered_pubkey.0);
-
-        msg!("Generated Recovered PubKey: {}", recovered_pubkey_hex);
-
-        if eth_pubkey == recovered_pubkey_hex {
-            msg!("Public Key Match!");
-        } else {
-            msg!("Public Key Doesn't Match!");
+        if data.message != msg_data_hashed.to_bytes() {
+            msg!("MSG Dont Match");
+            return err!(ValidationError::InvalidDataHash)
         }
-        // value from recovered_pubkey_hex:
-        // 58a8a3bd63db626497e1b4c09fb3119257c2bd0c651084eac0c038ad49bec874e380efbb5f8232aceae2383b9a1bbc22e936121ebaf83ff880801232327380e3
 
-        // eth_pubkey should equals to recovered_pubkey_hex ?
+        let mut is_match = false;
+
+        // Attempt recovery with both valid recovery IDs (0 and 1)
+        for recovery_id in [0, 1] {
+            let recovered_pubkey = secp256k1_recover(
+                &data.message,
+                recovery_id,
+                &data.signature
+            )
+            .map_err(|_| ProgramError::InvalidArgument)?;
+    
+            if &ETH_PUBKEY == &recovered_pubkey.0 {
+                is_match = true;
+            }
+        }
+
+        if !is_match {
+            msg!("Sig Not valid");
+            return err!(ValidationError::InvalidSignature);
+        }
 
         Ok(())
     }
+
 }
 
 #[derive(Accounts)]
 pub struct New {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct DataToVerify{
+    address: String,
+    amount: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct VerificationData {
+    message: Vec<u8>,
+    signature: [u8; 64],
+    verify_data: DataToVerify
+} 
+
+#[error_code]
+pub enum ValidationError {
+    #[msg("Message does not match 'verify_data' message hash")]
+    InvalidDataHash,
+    #[msg("Signature does not match public key")]
+    InvalidSignature,
+}
+
+#[event]
+pub struct DepositEvent {
+    pub sender: Pubkey,
+    pub amount: u64
+}
+
+#[derive(Accounts)]
+pub struct DepositCtx<'info> {
+    #[account(mut)]
+    payer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            b"treasury",
+        ],
+        bump,
+    )]
+    treasury: SystemAccount<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawCtx<'info> {
+    #[account(mut)]
+    /// CHECK: this is unsafe
+    receiver: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            b"treasury",
+        ],
+        bump,
+    )]
+    treasury: SystemAccount<'info>,
+    system_program: Program<'info, System>,
+}
