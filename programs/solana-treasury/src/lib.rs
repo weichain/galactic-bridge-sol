ffdebb00e43da9e34b9854643dfcb8b77c9d75c4
Teodor Zafirov <36457245+t3dkich@users.noreply.github.com>
2024-03-06 14:47:52 +0200

    feat: removed test denominator from contract and tests

--- programs/solana-treasury/src/lib.rs
+++ programs/solana-treasury/src/lib.rs
@@ -8,20 +8,21 @@ use deposit::*;
 pub mod withdraw;
 use withdraw::*;
 
-declare_id!("3PxEqHbGAWZthkyuJvtTjjwYSQ3HRGJVyk8MbjnZP2mt");
+declare_id!("HS6NTv6GBVSLct8dsimRWRvjczJTAgfgDJt8VpR8wtGm");
 
 #[program]
 pub mod solana_treasury {
     use super::*;
     
+    // 69b3e4f4295275d99217c8d4f31a872d7af55c671fde7b0ed293650f9d1a41155547644c9958888110145c9846d85863665d62398e53bed8f5727f5508cd0bb2
     const ETH_PUBKEY: [u8; 64] = [
-        103, 195,  88, 222,  59, 129,  88,  22, 117, 109, 111,
-         44, 176,  82,  12, 162,  43, 102,  11,  40,  45, 232,
-        229, 153, 222, 131, 162, 180, 187,  47,  96, 171, 145,
-        123, 189, 254,  81,  78, 224, 252,  29, 180, 157,  53,
-        206, 225,  54, 199,   6, 242, 238, 217,  95, 117, 170,
-         68,  71, 185, 108, 206, 181, 188, 172,  87
-    ];
+        105, 179, 228, 244,  41,  82, 117, 217, 146, 23, 200,
+        212, 243,  26, 135,  45, 122, 245,  92, 103, 31, 222,
+        123,  14, 210, 147, 101,  15, 157,  26,  65, 21,  85,
+         71, 100,  76, 153,  88, 136, 129,  16,  20, 92, 152,
+         70, 216,  88,  99, 102,  93,  98,  57, 142, 83, 190,
+        216, 245, 114, 127,  85,   8, 205,  11, 178
+      ];
 
     pub fn deposit(ctx: Context<DepositCtx>, data: DepositData) -> Result<()> {
         deposit::deposit(ctx, data)?;
